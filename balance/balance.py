4a76844120a0264cccf37cf2c661f9b334977252
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-09-09 16:43:44 +0100

    Create balance.py

---
+++ balance/balance.py
@@ -0,0 +1,48 @@
+from itertools import product
+from collections import defaultdict
+
+def count_up(p, q, r):
+    return product(*[range(p,q+1)]*r)
+
+def force_solve(lhs, rhs):
+    l = len(lhs) + len(rhs)
+    for mults in count_up(1, 10, l):
+        nl, nr = lhs.copy(), rhs.copy()
+        lm, rm = mults[:len(lhs)], mults[len(lhs):]
+        le, re = defaultdict(int), defaultdict(int)
+        for el, ml in zip(nl, lm):
+            for x in el.split():
+                en, n = x.split('_')
+                le[en] += int(n) * ml
+        for er, mr in zip(nr, rm):
+            for x in er.split():
+                en, n = x.split('_')
+                re[en] += int(n) * mr
+        if le == re:
+            return mults
+    return False
+
+def process(s):
+    r = []
+    for c in s:
+        if c.isupper():
+            if r and '_' not in r[-1]:
+                r[-1] += '_1'
+            r.append(c)
+        elif c.islower():
+            r[-1] += c
+        else:
+            r[-1] += '_' + c
+    if r and '_' not in r[-1]:
+        r[-1] += '_1'
+    return ' '.join(r)
+
+
+def interface(l, r):
+    ls, rs = l.replace(' ', '').split('+'), r.replace(' ', '').split('+')
+    sol = force_solve([process(x) for x in ls], [process(y) for y in rs])
+    if not sol:
+        return False
+    lm, rm = sol[:len(ls)], sol[len(ls):]
+    return ('+'.join(str(i)*(i>1)+j for i,j in zip(lm,ls)),
+            '+'.join(str(i)*(i>1)+j for i,j in zip(rm,rs)))
